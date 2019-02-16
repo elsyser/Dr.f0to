ad360036bbdae45c1d68bb3c4eb149e5f9178242
Victor Velev <velev.victor@yahoo.com>
2019-02-16 12:38:26 +0200

    [Update] ConvNet v1

--- deep-face/models/dataloader.py
+++ deep-face/models/dataloader.py
@@ -7,12 +7,13 @@ __all__ = [
     'LABELS_MAP',
 
     'parse_pixelstr',
+    'one_hot',
     'train_dev_test_sets',
 ]
 
 
 PATH_TO_IMAGES = '../data/fer/fer2013.csv'
-IMG_SHAPE = (48, 48)
+IMG_SHAPE = (48, 48, 1)
 LABELS_MAP = {
     0: 'Angry',
     1: 'Disgust',
@@ -28,6 +29,17 @@ def parse_pixelstr(pixelstr, shape=IMG_SHAPE):
     """Parse the data: A string of pixels to 2d array
     """
     return np.array(list(map(int, pixelstr.split(' ')))).reshape(shape)
+
+def one_hot(y):
+    """Performs One-Hot encoding
+    """
+    m = len(y)
+    n_classes = len(LABELS_MAP.values())
+
+    Y = np.zeros((m, n_classes))
+    Y[np.arange(m), y] = 1
+
+    return Y
 
 def train_dev_test_sets():
     """Performs train/dev/test set split and retuns the sets
@@ -50,8 +62,22 @@ def train_dev_test_sets():
             X_dev.append(parse_pixelstr(row['pixels']))
             y_dev.append(row['emotion'])
 
+    # Parse to np arrays
+    X_train = np.array(X_train)
+    X_dev = np.array(X_dev)
+    X_test = np.array(X_test)
+
+    # One-Hot encoding
+    Y_train = one_hot(y_train)
+    Y_dev = one_hot(y_dev)
+    Y_test = one_hot(y_test)
+
+    assert X_train.shape[0] == Y_train.shape[0]
+    assert X_dev.shape[0] == Y_dev.shape[0]
+    assert X_test.shape[0] == Y_test.shape[0]
+
     return (
-        (X_train, y_train),
-        (X_dev, y_dev),
-        (X_test, y_test),
+        (X_train, Y_train),
+        (X_dev, Y_dev),
+        (X_test, Y_test),
     )
--- deep-face/models/layers.py
+++ deep-face/models/layers.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 
 __all__ = [
-    'conv2d_layer',
-    'max_pool',
-    'dense_layer',
+    'conv2d',
+    'maxpool',
+    'fully_connected',
 ]
 
 
-def conv2d_layer(input_X, n_filters=3, size=[1, 1], strides=[1, 1, 1, 1], padding='VALID', dilations=[1, 1, 1, 1], name='conv2d-layer'):
+def conv2d(input_X, n_filters=3, size=[1, 1], strides=[1, 1, 1, 1], padding='VALID', dilations=[1, 1, 1, 1], name='conv2d-layer'):
     _, _, _, n_C = input_X.shape
 
-    with tf.name_scope(name) as nscope:
-        with tf.variable_scope(name+'-variables') as vscope:
+    with tf.name_scope(name):
+        with tf.variable_scope(name+'-variables'):
             W = tf.get_variable(name='W', shape=[*size, n_C, n_filters], initializer=tf.contrib.layers.xavier_initializer())
             b = tf.get_variable(name='b', shape=[1, 1, 1, n_filters], initializer=tf.initializers.zeros())
 
@@ -20,15 +20,15 @@ def conv2d_layer(input_X, n_filters=3, size=[1, 1], strides=[1, 1, 1, 1], paddin
 
     return Z
 
-def max_pool(input_X, size=[1, 1], strides=[1, 1, 1, 1], padding='VALID', name='max-pool-layer'):
-    with tf.name_scope(name) as nscope:
-        pool =  tf.nn.max_pool(input_X, ksize=[1, *size, 1], strides=strides, padding=padding, name='max-pool')
+def maxpool(input_X, size=[1, 1], strides=[1, 1, 1, 1], padding='VALID', name='maxpool-layer'):
+    with tf.name_scope(name):
+        pool =  tf.nn.max_pool(input_X, ksize=[1, *size, 1], strides=strides, padding=padding, name='maxpool')
         
     return pool
 
-def dense_layer(A_prev, units=1, activation=None, name='dense-layer'):
-    with tf.name_scope(name) as nscope:
-        with tf.variable_scope(name+'-variables') as vscope:
+def fully_connected(A_prev, units=1, activation=None, name='fc-layer'):
+    with tf.name_scope(name):
+        with tf.variable_scope(name+'-variables'):
             W = tf.get_variable(name='W', shape=[A_prev.shape[1], units], initializer=tf.initializers.he_normal())
             b = tf.get_variable(name='b', shape=[1, units], initializer=tf.initializers.he_normal())
 
